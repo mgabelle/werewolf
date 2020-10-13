398fb2b7d0d44211dadc78f532b2c55112035937
eagmabt <mathieu.gabelle@ericsson.com>
2020-10-13 10:02:40 +0200

    add hello world controller and build for maven

--- src/main/java/com/werewolf/backend/main/LaunchGame.java
+++ src/main/java/com/werewolf/backend/main/LaunchGame.java
@@ -1,21 +1,19 @@
 package com.werewolf.backend.main;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.*;
-
+@SpringBootApplication
 @RestController
-@EnableAutoConfiguration
 public class LaunchGame {
-    @RequestMapping("/")
-    String werewolfWelcome() {
-        return "Welcome to the werewolf App";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(LaunchGame.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello World";
     }
 }
